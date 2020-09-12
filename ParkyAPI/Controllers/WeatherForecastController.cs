938c863c1eabaeebcfb399b68222a1fdb9d5379a
saura <saura@DESKTOP-5MSCH3P>
2020-09-12 13:00:36 +0100

    Added UseSqlServer Added Migration Added logger in Program file

--- ParkyAPI/Controllers/WeatherForecastController.cs
+++ ParkyAPI/Controllers/WeatherForecastController.cs
@@ -27,6 +27,7 @@ namespace ParkyAPI.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            _logger.LogInformation("Got get request");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
